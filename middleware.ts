57c983355581495ed5a87abeaae1b68a8841acea
werhw <kleppeleo@gmail.com>
2024-02-22 10:51:43 +0100

    fixed middleware issue

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,6 @@ import { updateSession } from './app/__actions/authActions'
  
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
-  console.log('middleware')
+  console.log('middleware')   
   return await updateSession(request)
 }
