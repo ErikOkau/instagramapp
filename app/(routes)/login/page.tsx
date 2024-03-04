983a0412f672612207d2b8fb7de840290f79ea37
ErikOkau <erikmsorheim@yahoo.no>
2024-03-04 11:41:41 +0100

    Starting on login/register page

--- app/(routes)/login/page.tsx
+++ app/(routes)/login/page.tsx
@@ -3,12 +3,13 @@
 import { useState } from "react"
 import Login from "../../__components/login"
 import Register from "../../__components/register"
+import './login.scss'
 
 export default function async () {
     const [login, setLogin] = useState(true)
 
     return (
-        <div>
+        <div className="login">
             {login ? <Login /> : <Register />}
             <button onClick={() => setLogin(true)}>Login</button>
             <button onClick={() => setLogin(false)}>Register</button>
