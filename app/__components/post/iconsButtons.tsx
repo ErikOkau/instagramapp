8ff247a889ae1e9aef15b63403b09d87472694a4
werhw <kleppeleo@gmail.com>
2024-03-18 12:49:59 +0100

    Fix like button behavior and image styling

--- app/__components/post/iconsButtons.tsx
+++ app/__components/post/iconsButtons.tsx
@@ -1,21 +1,20 @@
 "use client"
 
-import { checkIfLiked, likePost } from "@/app/__actions/postActions"
+import { likePost } from "@/app/__actions/postActions"
 import { Like } from "@prisma/client"
 import { useState } from "react"
 
 
 export default function ({postId, likedCheck}: {postId?: number, likedCheck: Like | null | {error: string}}) {
     if(!postId) return (<div></div>)
-    const [liked, setLiked] = useState(!!likedCheck)
-
+    const [liked, setLiked] = useState(likedCheck?.hasOwnProperty("id") ? true : false)
     return (
         <div className="iconsButtons">
             <div>
                 <img src="/unselected_icon/heart.svg" data-liked={liked} onClick={async () => {
-                    const test = await likePost(postId)
+                    const likeRes = await likePost(postId)
+                    if(likeRes.error) return
                     setLiked(!liked)
-                    console.log(test)
                 }} />
                 <img src="/unselected_icon/share.svg" alt="" />
             </div>
