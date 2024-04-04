56d29e4aba36800424f757958f11291d8e0aaa0c
ErikOkau <erikmsorheim@yahoo.no>
2024-04-04 09:19:48 +0200

    Refactor create post page and add new upload icon

--- app/(routes)/post/create/page.tsx
+++ app/(routes)/post/create/page.tsx
@@ -2,19 +2,34 @@
 
 import { createPost } from "@/app/__actions/postActions"
 import SubmitButton from "@/app/__components/submitButton"
+import "./create.scss"
 
 export default function () {
-    return (
-        <form action={async formData => {
-            const response = await createPost(formData)
-            console.log(response)
-        }}>
-            <input type="file"  name="images" required multiple accept="image/png, image/gif, image/jpeg" />
-            <textarea name="content" placeholder="Content" required />
-            <SubmitButton 
-                text="Create"
-                pendingText="Creating..."
-            />
-        </form>
-    )
+  return (
+    <form
+      action={async (formData) => {
+        const response = await createPost(formData)
+        console.log(response)
+      }}
+    >
+      <div className="leftCreate">
+        <h1>Opprett nytt innlegg</h1>
+        <img src="/upload_create.svg" />
+        <input
+          type="file"
+          name="images"
+          required
+          multiple
+          accept="image/png, image/gif, image/jpeg"
+        />
+      </div>
+
+      <div className="lineBetween"></div>
+
+      <div className="rightCreate">
+        <textarea name="content" placeholder="Content" required />
+        <SubmitButton text="Create" pendingText="Creating..." />
+      </div>
+    </form>
+  )
 }
