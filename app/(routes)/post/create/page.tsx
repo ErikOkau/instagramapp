43af0fe37d8788a33ffa1062b6db7a1fc366e149
ErikOkau <erikmsorheim@yahoo.no>
2024-04-04 11:30:54 +0200

    lagde en helt sykt scuffed css for å gjemme "choose file" greiene for å kunne opplaste filer

--- app/(routes)/post/create/page.tsx
+++ app/(routes)/post/create/page.tsx
@@ -14,7 +14,17 @@ export default function () {
     >
       <div className="leftCreate">
         <h1>Opprett nytt innlegg</h1>
-        <img src="/upload_create.svg" />
+        <div className="inputImage">
+          <img src="/upload_create.svg" />
+          <input
+            className="hide_imageFile"
+            type="file"
+            name="images"
+            required
+            multiple
+            accept="image/png, image/gif, image/jpeg"
+          />
+        </div>
         <p>Dra bilder eller videoer her</p>
         <div className="inputBox">
           <p>Velg fra enheten din</p>
